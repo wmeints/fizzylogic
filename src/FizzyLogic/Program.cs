13d3bc28123555860212a65c005e8156b13d150d
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-12-31 14:41:12 +0100

    chore: Add XML comments to the main program entrypoint

--- src/FizzyLogic/Program.cs
+++ src/FizzyLogic/Program.cs
@@ -2,14 +2,26 @@ namespace FizzyLogic
 {
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
-
+    
+    /// <summary>
+    /// Main class for the application.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Entrypoint for the application.
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates the host builder for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
@@ -18,6 +30,5 @@ namespace FizzyLogic
                     _ = webBuilder.UseStartup<Startup>();
                 });
         }
-
     }
 }
