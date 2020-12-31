07bac752c45a93dfe89f21853917d002a4667c4d
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-12-31 14:38:41 +0100

    chore: Add XML comments to the tag helpers

--- src/FizzyLogic/TagHelpers/ValidationStateTagHelper.cs
+++ src/FizzyLogic/TagHelpers/ValidationStateTagHelper.cs
@@ -7,15 +7,34 @@
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
     using Microsoft.AspNetCore.Razor.TagHelpers;
 
+    /// <summary>
+    /// Use this tag helper to show/hide the is-valid class on a validated field.
+    /// </summary>
 
     [HtmlTargetElement("input", Attributes = "validation-state-for")]
     public class ValidationStateTagHelper : TagHelper
     {
-        [ViewContext] [HtmlAttributeNotBound] public ViewContext ViewContext { get; set; }
+        /// <summary>
+        /// Gets or sets the  view context for the tag helper.
+        /// </summary>
+        /// <value></value>
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
 
+        /// <summary>
+        /// Gets or sets the field to show the validation state for.
+        /// </summary>
+        /// <value></value>
         [HtmlAttributeName("validation-state-for")]
         public ModelExpression For { get; set; }
 
+        /// <summary>
+        /// Processes the user input and hows/hide the is-invalid css class 
+        /// on the element that this tag helper was used on.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext.ModelState.ContainsKey(For.Name))
