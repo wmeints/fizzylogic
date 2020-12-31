304a1e9a37578a8faafebe6063f0a3a649d927c7
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-12-31 11:55:18 +0100

    chore: Add XML comments for login form

--- src/FizzyLogic/Forms/LoginForm.cs
+++ src/FizzyLogic/Forms/LoginForm.cs
@@ -1,8 +1,18 @@
 ﻿namespace FizzyLogic.Forms
 {
+    /// <summary>
+    /// This form is used on the login page to provide login details.
+    /// </summary>
     public class LoginForm
     {
+        /// <summary>
+        /// Gets or sets the e-mail address to login with.
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password to login with.
+        /// </summary>
         public string Password { get; set; }
     }
 }
