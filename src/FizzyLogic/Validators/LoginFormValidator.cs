dc9586687212a4e946a469e09933eee766e58843
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-12-31 14:39:35 +0100

    chore: Add XML comments to the login form validator

--- src/FizzyLogic/Validators/LoginFormValidator.cs
+++ src/FizzyLogic/Validators/LoginFormValidator.cs
@@ -4,8 +4,14 @@ namespace FizzyLogic.Validators
     using FizzyLogic.Forms;
     using FluentValidation;
 
+    /// <summary>
+    /// Specifies validation rules for the <see cref="LoginForm">.
+    /// </summary>
     public class LoginFormValidator : AbstractValidator<LoginForm>
     {
+        /// <summary>
+        /// Initializes a new instanc of <see cref="LoginFormHelper"/>.
+        /// </summary>
         public LoginFormValidator()
         {
             _ = RuleFor(x => x.EmailAddress).EmailAddress().NotEmpty();
