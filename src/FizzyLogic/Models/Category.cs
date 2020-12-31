d3f8a8204c356295b4c63e9654c7e507616f67e7
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-12-31 12:01:37 +0100

    chore: Add XML comments for models

--- src/FizzyLogic/Models/Category.cs
+++ src/FizzyLogic/Models/Category.cs
@@ -1,9 +1,24 @@
 namespace FizzyLogic.Models
 {
+    /// <summary>
+    /// Each article is filed in a category. 
+    /// </summary>
     public class Category
     {
+        /// <summary>
+        /// Gets or sets the ID of the category.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slug for the article. This is used in the URL
+        /// to identify a category.
+        /// </summary>
         public string Slug { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the category.
+        /// </summary>
         public string Title { get; set; }
     }
 }
