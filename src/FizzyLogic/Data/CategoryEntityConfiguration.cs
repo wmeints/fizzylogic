932b707128f4b7e20fb5534c4b2c7b0fe0b547d5
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-12-31 11:54:00 +0100

    chore: Add XML comments for data context related classes

--- src/FizzyLogic/Data/CategoryEntityConfiguration.cs
+++ src/FizzyLogic/Data/CategoryEntityConfiguration.cs
@@ -3,8 +3,15 @@ namespace FizzyLogic.Data
     using FizzyLogic.Models;
     using Microsoft.EntityFrameworkCore;
 
+    /// <summary>
+    /// Provides model configuration for the <see cref="FizzyLogic.Models.Category"/> entity.
+    /// </summary>
     public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
     {
+        /// <summary>
+        /// Provides the model configuration for the entity.
+        /// </summary>
+        /// <param name="builder">Entity model builder instance.</param>
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Category> builder)
         {
             _ = builder.Property(x => x.Title).IsRequired().HasMaxLength(500);
