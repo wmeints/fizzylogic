531dbcdbccb93f6350d677f20d3ee4c36150f2da
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-01-04 11:47:29 +0100

    chore: Add tests for the image service

---
+++ src/FizzyLogic.Tests/ObjectMother.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzyLogic.Tests
+{
+    using FizzyLogic.Services;
+    using NSubstitute;
+    using System;
+
+    /// <summary>
+    /// Provides mocks and other useful tidbits of data for the tests.
+    /// </summary>
+    public class ObjectMother
+    {
+        /// <summary>
+        /// Creates a clock set to a fixed date/time
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static IClock StaticClock(DateTime date)
+        {
+            var staticClock = Substitute.For<IClock>();
+            _ = staticClock.UtcNow.Returns(date);
+
+            return staticClock;
+        }
+    }
+}
---
+++ src/FizzyLogic/Services/DefaultClock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FizzyLogic.Services
+{
+    /// <summary>
+    /// Default implementation of the application clock.
+    /// </summary>
+    public class DefaultClock : IClock
+    {
+        /// <inheritdoc/>
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
