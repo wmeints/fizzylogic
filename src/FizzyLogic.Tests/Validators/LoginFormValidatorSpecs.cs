197bd9ca8b07d587c6194c97d1bd19eaac19e5df
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-01-05 08:13:08 +0100

    chore: Add tests for LoginFormValidator

---
+++ src/FizzyLogic.Tests/Validators/LoginFormValidatorSpecs.cs
@@ -0,0 +1,60 @@
+﻿namespace FizzyLogic.Tests.Validators
+{
+    using Chill;
+    using FizzyLogic.Validators;
+    using FluentAssertions;
+    using FluentValidation.Results;
+    using System.Linq;
+    using Xunit;
+
+    public class LoginFormWhenEmailAddressIsNotSpecified : GivenSubject<LoginFormValidator, ValidationResult>
+    {
+        public LoginFormWhenEmailAddressIsNotSpecified()
+        {
+            WithSubject(resolver => new LoginFormValidator());
+
+            When(() => Subject.Validate(new Forms.LoginForm
+            {
+                EmailAddress = "",
+                Password = "123Test!"
+            }));
+        }
+
+        [Fact]
+        public void ThenReturnsEmailAddressEmptyError()
+        {
+            _ = Result.Errors
+                .FirstOrDefault(x => x.ErrorMessage == "'Email Address' must not be empty.")
+                .Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ThenReturnsEmailAddressInvalidError()
+        {
+            _ = Result.Errors
+                .FirstOrDefault(x => x.ErrorMessage == "'Email Address' is not a valid email address.")
+                .Should().NotBeNull();
+        }
+    }
+
+    public class LoginFormWhenPasswordIsNotSpecified : GivenSubject<LoginFormValidator, ValidationResult>
+    {
+        public LoginFormWhenPasswordIsNotSpecified()
+        {
+            WithSubject(resolver => new LoginFormValidator());
+            When(() => Subject.Validate(new Forms.LoginForm
+            {
+                EmailAddress = "test@domain.org",
+                Password = ""
+            }));
+        }
+
+        [Fact]
+        public void ThenPasswordEmptyErrorIsReturned()
+        {
+            _ = Result.Errors
+                .FirstOrDefault(x => x.ErrorMessage == "'Password' must not be empty.")
+                .Should().NotBeNull();
+        }
+    }
+}
