22413702dde98affa2a734594db0e903efce61f3
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-01-05 12:06:58 +0100

    feat: Add preview page for articles

---
+++ src/FizzyLogic/Pages/ArticlePreview.cshtml.cs
@@ -0,0 +1,41 @@
+﻿namespace FizzyLogic.Pages
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class ArticlePreviewPageModel : PageModel
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ArticlePreviewPageModel"/>.
+        /// </summary>
+        /// <param name="applicationDbContext">DbContext to load data.</param>
+        public ArticlePreviewPageModel(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// Gets or sets the article to render.
+        /// </summary>
+        public Article Article { get; set; }
+
+        /// <summary>
+        /// Handles the GET operation for the page.
+        /// </summary>
+        /// <param name="id">The ID for the article.</param>
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Article = await _applicationDbContext.Articles
+                .Include(x => x.Category)
+                .SingleOrDefaultAsync(x => x.Id == id);
+
+            return Article == null ? NotFound() : Page();
+        }
+    }
+}
